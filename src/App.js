0dadc26a2baa114e07e0e592692f42726ab1698c
maheswar <maheswarreddyappojigari@gmail.com>
2025-05-09 20:57:46 +0530

    added some minor features for Search Component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import StarRating from "./StarRating.js";
 
 function NavBar({ children }) {
@@ -18,13 +18,32 @@ function Logo() {
   );
 }
 function Search({ query, setQuery }) {
+
+  const inputEl=useRef(null)
+ 
+  useEffect(function(){
+  
+   function callback(e){
+    if(document.activeElement===inputEl.current) return;
+    if(e.code==="Enter"){
+inputEl.current.focus();
+    setQuery('')
+  }
+  }  
+document.addEventListener('keydown',callback);
+return ()=>document.addEventListener("keydown",callback)
+  },[setQuery])
+  console.log(inputEl)
   return (
     <input
       className="search"
       type="text"
       placeholder="Search movies..."
       value={query}
-      onChange={(e) => setQuery(e.target.value)}
+      onChange={(e) => setQuery(e.target.value)
+       
+      }
+      ref={inputEl} 
     />
   );
 }
